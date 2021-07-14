69058f2575ac83ae6d76e7a8078e4d90337a5c55
rajyadav101 <kumar.raj.raj101@gmail.com>
2021-07-14 22:19:15 +0530

    Init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Home Page</h1>
+  <p>component state{{count}}</p>
+  <p>store state{{this.$store.state.count}}</p>
+  <button @click="increment">Up vote</button>
+  <button @click="decriment">Down vote</button>
+</template>
+
+// JavaScript
+<script>
+import store from "../store";
+
+export default {
+  name: 'Home',
+  data() {
+    return { count: 4 }
+  },
+  methods: {
+    increment() {
+      // `this` will refer to the component instance
+       store.commit('increment')
+       console.log(this.$store.state.count)
+    },
+    decriment(){
+      this.count--
+    }
+  }
+  
+}
+console.log("hi there");
+</script>
---
+++ src/route/route.js
@@ -0,0 +1,27 @@
+
+import {createRouter, createWebHistory} from "vue-router";
+import HelloWorld from '../components/HelloWorld'
+import Home from '../components/Home'
+// 1. Define route components.
+
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', name:"Home", component: Home },
+  { path: '/foo', name:"Foo", component: HelloWorld },
+  { path: '/bar', name:"Bar", component: HelloWorld }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes, // short for `routes: routes`
+})
+
+export default router;
